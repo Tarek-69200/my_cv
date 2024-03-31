8c0b857ba96edbf1bd4770f3927c8905e2b6be2d
Tarek Taguine <t.taguine@it-students.fr>
2024-03-31 18:16:22 +0200

    v:2.5.5 - Add panel nav

--- backend/adminPanel.php
+++ backend/adminPanel.php
@@ -10,6 +10,23 @@
 
 <body>
 
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Paneau de navigation</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" href="adminPanel.php">Admin Panel</a>
+        <a class="nav-link" href="loginPanel.php">Login Panel</a>
+        <a class="nav-link" href="deleteMessage.php">Delete Message</a>
+        <a class="nav-link" href="logout.php">Logout</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
     <?php
 
     /**
---
+++ backend/deleteMessage.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+</head>
+<body>
+    <?php
+    require_once '../database/database.php';
+    echo '<h1>Admin Panel</h1>';
+    $sql = "SELECT * FROM messages";
+    $result = $conn->query($sql);
+    if ($result->rowCount() > 0) {
+        echo '<table class="table">';
+        echo '<thead>';
+        echo '<tr>';
+        echo '<th scope="col">Name</th>';
+        echo '<th scope="col">Message</th>';
+        echo '<th scope="col">Email</th>';
+        echo '<th scope="col">Date</th>';
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            echo '<tr>';
+            echo '<td>' . $row['name'] . '</td>';
+            echo '<td>' . $row['message'] . '</td>';
+            echo '<td>' . $row['email'] . '</td>';
+            echo '<td>' . $row['date'] . '</td>';
+            echo '</tr>';
+        }
+        echo '</tbody>';
+        echo '</table>';
+    }
+    ?>
+    <form action="deleteMessage.php" method="post">
+        <input type="text" name="id" placeholder="Message ID">
+        <button type="submit">Delete</button>
+    </form>
+    
+</body>
+</html>
