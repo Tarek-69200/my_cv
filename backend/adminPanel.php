fa0f2798d706db5664a007e066f7ec9aa3170fc6
Tarek Taguine <t.taguine@it-students.fr>
2024-03-31 17:53:52 +0200

    v:2.4.5 - review code

--- backend/adminPanel.php
+++ backend/adminPanel.php
@@ -5,19 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
     <title>Admin Panel</title>
 </head>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 <body>
 
     <?php
 
+    /**
+     * This file represents the admin panel page.
+     * It displays a table of messages retrieved from the database.
+     */
+
     require_once '../database/database.php';
     echo '<h1>Admin Panel</h1>';
-
 
     $sql = "SELECT * FROM messages";
 
@@ -25,10 +26,20 @@
 
     if ($result->rowCount() > 0) {
         echo '<table class="table">';
-        echo '<tr><th>Message</th><th>Email</th></tr>';
+        echo '<thead>';
+        echo '<tr>';
+        // echo '<th scope="col">#</th>';
+        echo '<th scope="col">Name</th>';
+        echo '<th scope="col">Message</th>';
+        echo '<th scope="col">Email</th>';
+        echo '<th scope="col">Date</th>';
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
 
         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
             echo '<tr>';
+            // echo '<th scope="row">' . $row['id'] . '</th>';
             echo '<td>' . $row['name'] . '</td>';
             echo '<td>' . $row['message'] . '</td>';
             echo '<td>' . $row['email'] . '</td>';
@@ -36,10 +47,12 @@
             echo '</tr>';
         }
 
+        echo '</tbody>';
         echo '</table>';
     }
     ?>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
