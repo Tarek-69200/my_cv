f28bfadf9984276449d6aaf5cf137002308a9df6
Tarek Taguine <t.taguine@it-students.fr>
2024-03-31 18:53:28 +0200

    v:2.6.6 - Add function TRIM

--- message.php
+++ message.php
@@ -3,9 +3,9 @@
 require_once './database/database.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
+    $name = trim(htmlspecialchars($_POST['name']));
+    $email = trim(htmlspecialchars($_POST['email']));
+    $message = trim(htmlspecialchars($_POST['message']));
     $sql = "INSERT INTO messages (name, email, message) VALUES (:name, :email, :message)";
     $stmt = $conn->prepare($sql);
     $stmt->execute(['name' => $name, 'email' => $email, 'message' => $message]);
