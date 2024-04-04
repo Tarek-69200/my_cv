e0fccb73887116403a01928798379d5f7764a644
Tarek Taguine <t.taguine@it-students.fr>
2024-04-04 13:08:04 +0200

    v:2.9.12 - fix bug send message empty

--- message.php
+++ message.php
@@ -6,11 +6,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = trim(htmlspecialchars($_POST['name']));
     $email = trim(htmlspecialchars($_POST['email']));
     $message = trim(htmlspecialchars($_POST['message']));
-    $sql = "INSERT INTO messages (name, email, message) VALUES (:name, :email, :message)";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute(['name' => $name, 'email' => $email, 'message' => $message]);
-    // Redirect to index.php 
-    header('Location: index.html');
-    exit;
+
+    if (empty($name) || empty($email) || empty($message)) {
+        echo "Veuillez remplir tous les champs du formulaire.";
+    } else {
+        $sql = "INSERT INTO messages (name, email, message) VALUES (:name, :email, :message)";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['name' => $name, 'email' => $email, 'message' => $message]);
+        // Redirect to index.php 
+        header('Location: index.html');
+        exit;
+    }
 }
 ?>
