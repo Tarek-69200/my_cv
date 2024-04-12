d5944df74760bb5712e2ae20bf589dcd4b85a3bf
Tarek Taguine <t.taguine@it-students.fr>
2024-04-12 22:16:27 +0200

    2.10.12 - fix patch sécurity

--- backend/logout.php
+++ backend/logout.php
@@ -2,13 +2,8 @@
 
 //Sessions destroy
 
-// session_start();
 session_destroy();
 // Unset all of the session variables
-
 setcookie('PHPSESSID', '', time() - 3600, '/');
 // Redirect to login page
-
-
-
 header('Location: login.php');
