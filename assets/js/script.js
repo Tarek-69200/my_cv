bb231c2b8a8b8b81a6ee18d504ab467d3eaf5404
Tarek Taguine <t.taguine@it-students.fr>
2024-04-18 21:29:05 +0200

    Ajouter des fonctionnalités de menu déroulant dans script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-//Script
+function toggleMenu() {
+    var x = document.getElementById("menuLinks");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
+
+  // Used to toggle the menu on small screens when clicking on the menu button
+  function openNav() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
