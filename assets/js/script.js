15db37076dce78394518914b0f98ddd522adca42
Tarek Taguine <t.taguine@it-students.fr>
2024-04-18 21:56:06 +0200

    remove css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,13 @@
-function toggleMenu() {
-    var x = document.getElementById("menuLinks");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
-
-  // Used to toggle the menu on small screens when clicking on the menu button
-  function openNav() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
+// function toggleMenu() {
+//     var x = document.getElementById("menuLinks");
+//     if (x.className.indexOf("w3-show") == -1) {
+//       x.className += " w3-show";
+//     } else {
+//       x.className = x.className.replace(" w3-show", "");
+//     }
+//     //click on menu link, hide menu and go to section
+//     x.addEventListener('click', function(){
+//       x.className = x.className.replace(" w3-show", "");
+//     });
+    
+//   }
