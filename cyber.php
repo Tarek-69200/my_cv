2c90a940598cc82b0b72ed539371757ec07b05b8
Tarek Taguine <t.taguine@it-students.fr>
2024-04-24 16:13:54 +0200

    Mise à jour du fichier sitemap.xml avec une nouvelle URL

--- cyber.php
+++ cyber.php
@@ -1,11 +1,9 @@
-
-
 <!DOCTYPE html>
 <html lang="fr">
 <!-- Head -->
 
 <head>
-<title>Cybersécurité , DevSecOps, Cyber, Rhône, Lyon 1, Lyon 2, Lyon 3, Lyon 4, Lyon 5, Lyon 6,</title>
+  <title>Cybersécurité , DevSecOps, Cyber, Rhône, Lyon 1, Lyon 2, Lyon 3, Lyon 4, Lyon 5, Lyon 6,</title>
   <link rel="icon" type="image/x-icon" href="favicon.png">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,7 +11,7 @@
   <meta name="keywords" content=" Taguine Tarek, DevSecOps, DevOps, Cyber Sécurité, Rhône, Lyon, France">
   <meta name="author" content="Taguine Tarek">
   <meta name="google-site-verification" content="puEyUWPXIZDB1jr8YMNu99xhJFw10rOUpPADVcjn2s4">
-  
+
 
   <link rel="stylesheet" href="./assets/css/main.css">
   <link rel="stylesheet" href="./assets/css/picture.css">
@@ -29,15 +27,14 @@
     <div class="w3-display-bottomleft w3-padding-large w3-opacity">
       <h1 class="w3-xxlarge">Taguine Tarek</h1>
       <img class="img" loading="lazy" src="./assets/images/tarek_taguine.webp" alt="Taguine Tarek Nordine">
-        </div>
+    </div>
   </header>
 
   <div class="w3-top">
     <div class="w3-bar w3-white w3-padding w3-card" style="letter-spacing:4px;">
       <a href="./index.php" class="w3-bar-item w3-button">Taguine Tarek</a>
       <div class="w3-right w3-hide-large w3-hide-medium">
-        <a href="./index.php" class="w3-bar-item w3-button w3-padding-large" title="Toggle Navigation Menu"
-          onclick="myFunction(); return false;">
+        <a href="./index.php" class="w3-bar-item w3-button w3-padding-large" title="Toggle Navigation Menu" onclick="myFunction(); return false;">
           <i class="fa fa-bars"></i>
           <span class="w3-hide-large w3-hide-medium">Menu</span>
 
@@ -45,7 +42,7 @@
       </div>
 
       <div id="menuLinks" class="w3-hide-small">
-      <a href="./index.php" class="w3-bar-item w3-button">Mes projets</a>
+        <a href="./index.php" class="w3-bar-item w3-button">Mes projets</a>
         <a href="./cv.html" class="w3-bar-item w3-button">CV</a>
         <a href="#one" class="w3-bar-item w3-button">Mes compétences</a>
         <a href="#three" class="w3-bar-item w3-button">Contact</a>
@@ -54,8 +51,7 @@
   </div>
 
   <!-- Sidebar on small screens when clicking the menu icon -->
-  <nav class="w3-sidebar w3-bar-block w3-white w3-card w3-animate-left w3-hide-medium w3-hide-large"
-    style="display:none" id="mySidebar">
+  <nav class="w3-sidebar w3-bar-block w3-white w3-card w3-animate-left w3-hide-medium w3-hide-large" style="display:none" id="mySidebar">
     <a href="./index.php" onclick="myFunction()" class="w3-bar-item w3-button">Accueil</a>
     <a href="#one" onclick="myFunction()" class="w3-bar-item w3-button">Mes compétences</a>
     <a href="./cv.html" onclick="myFunction()" class="w3-bar-item w3-button">CV</a>
@@ -72,10 +68,11 @@
         Lyon, Auvergne-Rhône-Alpes, France
       </p>
       <p>
-      Passionné par le monde de l'Open Source, j'ai décidé de me spécialiser dans l'administration système.
-              J'ai acquis des compétences dans la gestion de serveurs Linux, la configuration de services réseau, la
-              sécurisation des systèmes et la virtualisation. Je suis particulièrement à l'aise avec Debian et ses
-              dérivées.
+        J'ai orienté ma carrière vers le DevSecOps, afin d'acquérir une solide expertise dans la gestion de serveurs Linux, la configuration des services réseau, la sécurisation des systèmes et la virtualisation. J'ai une préférence pour Debian et ses dérivées, ce qui me donne une base solide pour aborder des projets complexes.
+
+        Dans mon approche, je suis guidé par les recommandations de l'Agence Nationale de la Sécurité des Systèmes d'Information (ANSSI) et de l'Open Web Application Security Project (OWASP). Je veille à intégrer les meilleures pratiques de sécurité dans toutes les étapes du développement, conformément aux normes établies.
+
+        Je réalise régulièrement des audits de sécurité pour évaluer les risques et détecter d'éventuelles vulnérabilités. Ces audits, alignés sur les principes de l'OWASP et les directives de l'ANSSI, m'aident à identifier les zones à risque et à mettre en place des mesures préventives pour renforcer la sécurité de mes applications, garantissant ainsi la protection des données sensibles.
       </p>
       <a href="#one" class="w3-button w3-black w3-round-xlarge">Mes compétences</a>
     </div>
@@ -84,52 +81,46 @@
   <div id="one" class="w3-row-padding w3-padding-64">
     <div class="w3-half">
       <div class="w3-container">
-      <h2>Système d'exploitation</h2>
-      <ul>
-              <li>Windows 11, 10</li>
-              <li>macOS</li>
-              <li>GNU/Linux</li>
-              <li>Configuration de services réseau</li>
-              <li>Sécurisation des systèmes</li>
-              <li>Virtualisation</li>
-              <li>Debian et ses dérivées</li>
-              <li>Gestion de log serveur</li>
-            </ul>
-            
+        <h2>Système d'exploitation</h2>
+        <ul>
+          <li>Windows 11, 10</li>
+          <li>macOS</li>
+          <li>GNU/Linux</li>
+          <li>Configuration de services réseau</li>
+          <li>Sécurisation des systèmes</li>
+          <li>Virtualisation</li>
+          <li>Debian et ses dérivées</li>
+          <li>Gestion de log serveur</li>
+        </ul>
+
       </div>
     </div>
     <div class="w3-row-padding w3-padding-64">
       <div class="w3-container">
-      <h2>Back-End</h2>
-      <p>Les technologies Back-End que j'utilise notamment dans le développement d'API REST.</p>
-            <ul>
-              <li>Node.js</li>
-              <li>PHP Symfony</li>
-              <li>Express.JS</li>
-              <li>Flask / Django</li>
-            </ul>
-        <br>
-        <h2>DevOps</h2>
-            <ul>
-              <li>CI/CD</li>
-              <li>Automatisation des tests</li>
-              <li>Orchestration de conteneurs</li>
-              <li>Infrastructure as Code</li>
-              <li>Monitoring et supervision</li>
-              <li>GitHub Actions / GitLab CI / Jenkins / Travis CI</li>
-              <li><a href="https://docs.oracle.com/en-us/iaas/Content/devops/using/home.htm">En savoir plus sur le
-                  DevOps</a></li>
-            </ul>
+        <h2>Offensive</h2>
+        <p>Les outils que j'utilise notamment pour le pentest.</p>
+        <ul>
+          <li>Nmap</li>
+          <li>Metasploit</li>
+          <li>Wireshark</li>
+          <li>John the Ripper</li>
+          <li>Aircrack-ng</li>
+          <li>Hydra</li>
+          <li>Nikto</li>
+          <li>Sqlmap</li>
+        </ul>
 
-         <h2>DevSecOps</h2>
-            <ul>
-              <li>Automatisation de la sécurité</li>
-              <li>Tests de sécurité automatisés</li>
-              <li>Intégration de la sécurité dans le cycle de vie du développement</li>
-              <li>Formation à la sécurité</li>
-              <li>Conformité réglementaire</li>
-              <li><a href="https://www.owasp.org/index.php/DevSecOps">En savoir plus sur le DevSecOps</a></li>
-            </ul>
+        <br>
+        <h2>Gouvernance</h2>
+        <ul>
+          <li>Politiques et procédures de sécurité</li>
+          <li>Gestion des risques</li>
+          <li>Conformité aux réglementations</li>
+          <li>Protection des données</li>
+          <li>Plan de continuité des activités</li>
+          <li>Audit et conformité</li>
+          <li>Responsabilité et responsabilisation</li>
+        </ul>
 
       </div>
     </div>
